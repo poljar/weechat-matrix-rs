14762829273c3e31d0ff919e35999a85890911c9
Damir Jelić <poljar@termina.org.uk>
2020-04-29 18:11:39 +0200

    weechat-matrix: Add the missing debug module.

---
+++ src/debug.rs
@@ -0,0 +1,27 @@
+use std::io;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use weechat::Weechat;
+
+#[derive(Clone)]
+pub struct Debug();
+
+impl Debug {
+    pub async fn write_helper(message: Vec<u8>) {
+        let message = String::from_utf8(message).unwrap();
+        let message =
+            Weechat::execute_modifier("color_decode_ansi", "1", &message)
+                .unwrap();
+        Weechat::print(&message)
+    }
+}
+
+impl io::Write for Debug {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        Weechat::spawn_from_thread(Debug::write_helper(buf.to_owned()));
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
