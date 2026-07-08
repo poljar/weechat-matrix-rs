56f61c8ae66b09e52d0c228a2299ab71ae3bb70c
Darafei Praliaskouski <me@komzpa.net>
2026-07-08 17:04:57 +0400

    test: merge room test modules

--- src/room/mod.rs
+++ src/room/mod.rs
@@ -105,24 +105,6 @@ fn restored_prev_batch(prev_batch: Option<String>) -> Option<PrevBatch> {
     prev_batch.map(PrevBatch::Backwards)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{restored_prev_batch, PrevBatch};
-
-    #[test]
-    fn restored_rooms_fetch_history_backwards_from_prev_batch() {
-        assert_eq!(
-            restored_prev_batch(Some("token".to_owned())),
-            Some(PrevBatch::Backwards("token".to_owned()))
-        );
-    }
-
-    #[test]
-    fn restored_rooms_without_prev_batch_have_no_history_request() {
-        assert_eq!(restored_prev_batch(None), None);
-    }
-}
-
 impl Deref for RoomHandle {
     type Target = MatrixRoom;
 
@@ -1382,6 +1364,19 @@ mod tests {
 
     fn text_content(body: &str) -> RoomMessageEventContent {
         RoomMessageEventContent::text_plain(body)
+    }
+
+    #[test]
+    fn restored_rooms_fetch_history_backwards_from_prev_batch() {
+        assert_eq!(
+            restored_prev_batch(Some("token".to_owned())),
+            Some(PrevBatch::Backwards("token".to_owned()))
+        );
+    }
+
+    #[test]
+    fn restored_rooms_without_prev_batch_have_no_history_request() {
+        assert_eq!(restored_prev_batch(None), None);
     }
 
     #[test]
